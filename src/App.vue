a791c1ea1ffd760a7feb6fd20dd669fac7ef078c
Brian Caldera <88503893+BrianJCal99@users.noreply.github.com>
2026-01-08 01:22:19 +1100

    Added Vercel Web Analytics

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterView } from 'vue-router'
 import NavBarComponent from './components/NavBarComponent.vue'
 import HomeHeaderComponent from './components/HomeHeaderComponent.vue'
 import HomeFooterComponent from './components/HomeFooterComponent.vue'
+import { Analytics } from "@vercel/analytics/react"
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import HomeFooterComponent from './components/HomeFooterComponent.vue'
   <HomeHeaderComponent />
   <RouterView />
   <HomeFooterComponent />
+  <Analytics />
 </template>
